60d8d57a37bc121de8f3a75ce606c4d4c3d627cf
10763215 <116775397+10763215@users.noreply.github.com>
2023-02-22 19:41:16 -0700

    Strings in C#

--- Start/Overview/GarbageCollect/Program.cs
+++ Start/Overview/GarbageCollect/Program.cs
@@ -1,7 +1,8 @@
 ﻿// Exercise file for LinkedIn Learning Course .NET Programming with C# by Joe Marini
 // Demonstration of Garbage Collection
 
-void DoSomeBigOperation() {
+void DoSomeBigOperation()
+{
     // create a large memory allocation that's only used in this function
     byte[] myArray = new byte[1000000];
 
@@ -16,7 +17,7 @@ Console.ReadLine();
 // Call the function that allocates a large memory chunk
 DoSomeBigOperation();
 // TODO: After the function completes, force a Garbage Collection 
-
+GC.Collect();
 // Retrieve and print the updated total memory amount
 Console.WriteLine($"Allocated memory is: {GC.GetTotalMemory(false)}");
 Console.ReadLine();
--- Start/Strings/Manipulation/Program.cs
+++ Start/Strings/Manipulation/Program.cs
@@ -4,23 +4,35 @@
 string str1 = "The quick brown fox jumps over the lazy dog.";
 string str2 = "This is a string";
 string str3 = "THIS is a STRING";
-string[] strs = {"one", "two", "three", "four"};
+string[] strs = { "one", "two", "three", "four" };
 
 // TODO: Length of a string 
-
+// Console.WriteLine(str1.Length);
 
 // TODO: Access individual characters
-
+// Console.WriteLine(str1[14]);
 
 // TODO: Iterate over a string like any other sequence of values
-
+// foreach (char ch in str1)
+// {
+//     Console.Write(ch);
+//     if (ch == 'b')
+//     {
+//         Console.WriteLine();
+//         break;
+//     }
+// }
 
 // TODO: String Concatenation         
-string outstr;
-
+// string outstr;
+// outstr = String.Concat(strs);
+// Console.WriteLine(outstr);
 
 // TODO: Joining strings together with Join
-
+// outstr = String.Join('.', strs);
+// Console.WriteLine(outstr);
+// outstr = String.Join("---", strs);
+// Console.WriteLine(outstr);
 
 // TODO: String Comparison
 
@@ -32,6 +44,9 @@ string outstr;
 // < 0 : first string comes before second in sort order
 // 0 : first and second strings are same position in sort order
 // > 0 : first string comes after the second in sort order
-
+// int result = String.Compare(str2, "This is a string");
+// Console.WriteLine(result);
 
 // TODO: Replacing content
+string outstr = str1.Replace("fox", "cat");
+Console.WriteLine($"{outstr}");
